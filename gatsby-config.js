0dcc68df8b6552c17fefb5c56ce0611d9eebdc0f
alex jenter <alex@attribute.ch>
2018-06-26 21:54:48 +0200

    ordered gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
+  pathPrefix: `/kontext`,
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
   plugins: ['gatsby-plugin-react-helmet'],
-  pathPrefix: `/kontext`,
 }
